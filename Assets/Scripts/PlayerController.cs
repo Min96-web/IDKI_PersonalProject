0d933e0c37a3e82a735021469cad809d2815df67
minhuizhong <minhui.zhong2015@gmail.com>
2024-10-22 17:17:27 +0800

    Player modifications

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed =25.0f;
+    private float turnSpeed = 25.0f; 
+    private float horizontalInput;
+
+    private float forwardInput;
+
+    public bool gameOver = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         
+        //Move the Vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //We turn the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    { 
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+        }
     }
 }
